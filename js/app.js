520a68a6395a9dd581ffe7ed23985524ad2ba123
Maw Shin Koh <mawshin@hotmail.sg>
2020-12-27 19:06:30 +0800

    update required field validation

--- js/app.js
+++ js/app.js
@@ -11,11 +11,23 @@ $(function () {
     	}
     });
 
+    function requiredField(element) {
+		element.find('.required-field').attr('required', 'required');
+	}
+
+	function requiredFieldHidden(element) {
+		element.find('.required-field').removeAttr('required');
+	}
+
     $('[name^="additional_guest_diet_preference"]').on('change', function() {
     	if($(this).val() === "Kid's meal" || $(this).val() === "Toddler (would not need to cater food)") {
     		$(this).parent().next().next().show();
+
+    		requiredField($(this).parent().next().next());
     	} else {
     		$(this).parent().next().next().hide();
+
+    		requiredFieldHidden($(this).parent().next().next());
     	}
     });
 
@@ -26,15 +38,41 @@ $(function () {
     		switch ($(this).val()) { 
 				case '1 additional guest': 
 					$('.guest1').show();
+
+					$('.guest2, .guest3, .guest4').hide();
+
+					requiredField($('.guest1'));
+					requiredFieldHidden($('.guest2'));
+					requiredFieldHidden($('.guest3'));
+					requiredFieldHidden($('.guest4'));
 					break;
 				case '2 additional guests': 
 					$('.guest1, .guest2').show();
+
+					$('.guest3, .guest4').hide();
+
+					requiredField($('.guest1'));
+					requiredField($('.guest2'));
+					requiredFieldHidden($('.guest3'));
+					requiredFieldHidden($('.guest4'));
 					break;
 				case '3 additional guests': 
 					$('.guest1, .guest2, .guest3').show();
+
+					$('.guest4').hide();
+
+					requiredField($('.guest1'));
+					requiredField($('.guest2'));
+					requiredField($('.guest3'));
+					requiredFieldHidden($('.guest4'));
 					break;		
 				case '4 additional guests': 
 					$('.guest1, .guest2, .guest3, .guest4').show();
+
+					requiredField($('.guest1'));
+					requiredField($('.guest2'));
+					requiredField($('.guest3'));
+					requiredField($('.guest4'));
 					break;
 			}
 
@@ -45,21 +83,19 @@ $(function () {
 
     function submitForm() {
 	    $.ajax({
-	        url:'//api.apispreadsheets.com/data/5636/',
-	        type:'post',
-	        data:$("#guest-form").serializeArray(),
+	        url: '//api.apispreadsheets.com/data/5636/',
+	        type: 'post',
+	        data: $('#guest-form').serializeArray(),
 	        success: function(){
-	            alert("Your RSVP has been submitted! We look forward to meeting you on our big day!");
+	            alert('Your RSVP has been submitted! We look forward to meeting you on our big day!');
 	        },
 	        error: function(){
-	            alert("There was an error, please try to submit the form later.");
+	            alert('There was an error, please try to submit the form later.');
 	        }
 	    });
 	}
 
 	$('#form-submit').click(function() {
-		if ($("#guest-form").valid()) { 
-            submitForm();
-        }
+        submitForm();
 	});
 });
